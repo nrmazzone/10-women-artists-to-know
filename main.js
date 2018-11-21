c77e8f46edd25085d48190988655ef1c9c47090a
nrmazzone <nrmazzone@gmail.com>
2018-11-21 15:50:03 -0500

    some java script not right

---
+++ main.js
@@ -0,0 +1,21 @@
+$('.artemisia1 ').on('mouseover',function(event){
+    // console.log('hello');
+    $('.image1-r artemisia').css('opacity:0.3;');
+  
+  })
+
+  $('.artemisia1').on('click',function(event){
+    $('.image1-r artemisia').css('box-shadow: 3px 3px 5px grey');
+
+})
+
+
+
+// $('.orange ').on('click',function(event){
+//    $('.box').css('background-color','orange');
+
+// })
+
+
+
+
