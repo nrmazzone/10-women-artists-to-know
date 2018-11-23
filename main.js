92554c93b4fbbf82b0ee4b99c72021fe8c7af166
nrmazzone <nrmazzone@gmail.com>
2018-11-23 11:42:59 -0500

    overly somewhat working

--- main.js
+++ main.js
@@ -1,21 +1,50 @@
-$('.artemisia1 ').on('mouseover',function(event){
-    // console.log('hello');
-    $('.image1-r artemisia').css('opacity:0.3;');
-  
-  })
 
-  $('.artemisia1').on('click',function(event){
-    $('.image1-r artemisia').css('box-shadow: 3px 3px 5px grey');
+// show modal
+const shouldShowModal = true;
+// console.log(123);
 
+$('.image1-r artemisia button, .close').on('click',function(){
+    $('.overlay').toggleClass('hidden');
 })
 
+// get modal element
+// var modal= document.getElementsByClassName("modal-container-a1");
+//  var img = document.getElementById('artemesia1');
+//  var modalImg = document.getElementById('img1');
+//  var captionText = document.getElementById('caption');
+
+$('.image1-r artemisia button').on('click',function(){
+    const $overlay = $('<div>').addClass('overlay').appendTo('body')
+    const $modalContainerA1 = $('Div').addClass('modal-container-a1').appendTo($overlay)
+   
+    $('#img1').appendTo($modalContainerA1)
+    $('<p>').text('Judith Beheading Holofernes')
+    $('<button>').text('close').appendTo($modalContainerA1).on('click', function(){
+        $overlay.remove()
+    })
+})
+
+if (!buttonShouldDisplay){
+    $('.buttonholder').remove()
+}
+if (!shouldShowModal){
+    $('.overlay').remove()
+}
 
 
-// $('.orange ').on('click',function(event){
-//    $('.box').css('background-color','orange');
-
-// })
 
 
 
 
+//  img.onclick = function(){
+//      modal.style.display = "block";
+//      modalImg.scr = this.scr;
+//      captionText.innerHTML = this.addEventListener;
+//  }
+// // get span element that closes the modal
+//  var span = document.getElementsByClassName("close") [0];
+
+// //  when user cliclks on span x close modal
+// span.onclick = function(){
+//     modal.style.diplay = "none";
+// }
